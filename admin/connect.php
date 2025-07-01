153f8fea4f0ebdaeae7c1092d2215b456db31333
haole0810 <letanhao0810@gmail.com>
2025-07-01 15:57:56 +0700

    fix

--- admin/connect.php
+++ admin/connect.php
@@ -1,14 +1,13 @@
 <?php
-    $port = "3306";
-    $host = "localhost";
-    $user = "root";
-    $pass = "12345";
-    $dbname = "cutepet2";
+$port = "3306";
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "cutepet2";
 
-    $link = new mysqli($host, $user, $pass, $dbname, $port);
-    $link->set_charset("utf8");
+$link = new mysqli($host, $user, $pass, $dbname, $port);
+$link->set_charset("utf8");
 
-    if ($link->connect_error) {
-        die("Kết nối thất bại: " . $link->connect_error);
-    }
-?>
+if ($link->connect_error) {
+    die("Kết nối thất bại: " . $link->connect_error);
+}
